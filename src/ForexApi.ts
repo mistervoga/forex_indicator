9501e21e8666aaabdea2d245f8cf775c23763937
Nicolas von Garrel <mistervoga@gmail.com>
2023-10-10 13:28:12 +0200

    add openexchangerates

--- src/ForexApi.ts
+++ src/ForexApi.ts
@@ -1,23 +1,35 @@
 // ForexApi.ts
 
 interface ForexApiResponse {
-  price: number; // Assuming the API response has a 'price' property of type number
-  // You can define other properties if they exist in the API response
+  price: number;
 }
 
 export async function fetchForexPrice(pair: string): Promise<number> {
-  // Replace with your actual forex data API URL
-  const apiUrl = `https://api.example.com/forex?pair=${pair}`;
+  const apiKey = "9f9d64e9cd084b74ac22e3972ec0c1f5"; // Replace with your API key
+
+  // Construct the API URL for the specific currency pair
+  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=USD`;
 
   try {
     const response = await fetch(apiUrl);
 
+    console.log(response.json());
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response was not ok (${response.status}: ${response.statusText})`
+      );
     }
 
-    const data: ForexApiResponse = await response.json(); // Type assertion to ForexApiResponse
-    return data.price;
+    const data = await response.json();
+
+    // Extract the price for the specified currency pair
+    const price = data.rates[pair];
+
+    if (typeof price !== "number") {
+      throw new Error(`Price data for ${pair} not available.`);
+    }
+
+    return price;
   } catch (error: any) {
     throw new Error(`Error fetching forex data: ${error.message}`);
   }
